e804e76a2c65be4ff1cc835c35bdae1bcb9b1058
Lucas Valdez <lnvaldez24@gmail.com>
2024-07-19 10:26:29 -0400

    Create 0009.py

---
+++ 0009.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x_str = str(x)
+
+        if x_str == x_str[::-1]:
+            return True 
+        else:
+            return False 
