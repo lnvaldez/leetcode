929f63eca84706d567864a70552fb80c1fd7be66
Lucas Valdez <lnvaldez24@gmail.com>
2024-07-19 10:26:51 -0400

    Create 0011.py

---
+++ 0011.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+        
+        while left < right:
+            width = right - left
+            container_height = min(height[left], height[right])
+            area = width * container_height
+            
+            max_area = max(max_area, area)
+            
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return max_area
+
