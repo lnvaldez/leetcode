6104f568f0905c082ead7749ef704e0e4b24c451
Lucas Valdez <lnvaldez24@gmail.com>
2024-08-20 14:20:33 -0400

    Create 1662.py

---
+++ 1662.py
@@ -0,0 +1,14 @@
+from typing import List 
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        w1 = ''.join(word1)
+        w2 = ''.join(word2)
+
+        if w1 == w2:
+            return True
+        
+        return False
+
+s = Solution()
+s.arrayStringsAreEqual(word1 = ["ab", "c"], word2 = ["a", "bc"])
