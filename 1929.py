67cb587ef6dbcbd30e9f659928c214436f24549f
Lucas Valdez <lnvaldez24@gmail.com>
2024-08-20 14:32:53 -0400

    Create 1929.py

---
+++ 1929.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        for i in range(n):
+            nums.append(nums[i])
+
+        return nums 
+
