2a5fa66570120142282c546cbcc16049574ef37e
Lucas Valdez <lnvaldez24@gmail.com>
2024-08-23 18:58:03 -0400

    Create 1913.py

--- 1913.py
+++ 1913.py
@@ -1,4 +1,3 @@
-from typing import List 
 class Solution():
     def maxProductDifference(self, nums: List[int]) -> int:
         l = []
@@ -11,5 +10,3 @@ class Solution():
                 nums.remove(min(nums))
         return ((l[0] * l[1]) - (l[2] * l[3]))
 
-s = Solution()
-print(s.maxProductDifference([5,6,2,7,4]))
